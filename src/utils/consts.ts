e1d3665de6bfcd4b8ff3023b666d303f515ab262
jofjof <116435248+jofjof@users.noreply.github.com>
2024-06-15 17:15:01 +0300

    Add VITE_SERVER environment variable and update API_BASE

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,7 +1,7 @@
 import data from './defaultPresets.json';
 import { filterSchema } from './api.interfaces';
 
-export const API_BASE = 'http://127.0.0.1:80';
+export const API_BASE = import.meta.env.VITE_SERVER ?? 'http://127.0.0.1:80';
 
 export const top_left_image = 'https://www.the-sun.com/wp-content/uploads/sites/6/2022/01/NINTCHDBPICT000283898937.jpg';
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    host: '0.0.0.0'
+  }
+  
 })
