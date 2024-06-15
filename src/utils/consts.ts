3ed1c9f711fb65ed9bd36d4c01605623a0793aba
jofjof <116435248+jofjof@users.noreply.github.com>
2024-06-15 17:17:01 +0300

    docker stuff

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,8 +1,11 @@
 import data from './defaultPresets.json';
 import { filterSchema } from './api.interfaces';
 
-export const API_BASE = import.meta.env.VITE_SERVER ?? 'http://127.0.0.1:80';
+export const API_BASE = import.meta.env.MODE === 'development' ? 'http://localhost:80' : '';
 
 export const top_left_image = 'https://www.the-sun.com/wp-content/uploads/sites/6/2022/01/NINTCHDBPICT000283898937.jpg';
 
 export const defaultPresets: { [key: string]: filterSchema[] } = data;
+
+// dev = development
+// build = production
