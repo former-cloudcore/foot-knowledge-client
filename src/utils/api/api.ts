735285a4574bf69318ed949bfa810e91b32da60a
eyal-hl <eyalhermonilahat@gmail.com>
2024-08-17 13:38:43 +0300

    oops

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -65,7 +65,7 @@ export const add_player_to_scoreboard = async (board: string, score_row: inputSc
 
 
 export const fetch_connections = async (player_id1: string, player_id2: string): Promise<connection_details[]> => {
-	const response = await customFetch(`${API_BASE}/games/connect_players/connection_details?player1=${player_id1}&player2=${player_id2}`);
+	const response = await customFetch(`/games/connect_players/connection_details?player1=${player_id1}&player2=${player_id2}`);
 
 	return response.json();
 };
@@ -76,7 +76,7 @@ export const fetch_path = async (
 	players_to_ignore?: string[],
 	teams_to_ignore?: string[],
 ): Promise<playerSchema[]> => {
-	const url: string = `${API_BASE}/games/connect_players/path?player1=${player_id1}&player2=${player_id2}`;
+	const url: string = `/games/connect_players/path?player1=${player_id1}&player2=${player_id2}`;
 	if (players_to_ignore) url.concat(`&players_to_ignore=${players_to_ignore.join(',')}`);
 	if (teams_to_ignore) url.concat(`&teams_to_ignore=${teams_to_ignore.join(',')}`);
 	const response = await customFetch(url);
@@ -85,25 +85,25 @@ export const fetch_path = async (
 };
 
 export const mass_fetch_connections = async (player_id: string, players: string[]): Promise<mass_connection_details[]> => {
-	const response = await customFetch(`${API_BASE}/games/connect_players/mass_connection_details?player_id=${player_id}&players=${players.join(',')}`);
+	const response = await customFetch(`/games/connect_players/mass_connection_details?player_id=${player_id}&players=${players.join(',')}`);
 
 	return response.json();
 };
 
 export const fetch_all_players_from_league_year = async (league_id: string, year: string): Promise<playerSchema[]> => {
-	const response = await customFetch(`${API_BASE}/players/league/${league_id}/${year}`);
+	const response = await customFetch(`/players/league/${league_id}/${year}`);
 
 	return response.json();
 };
 
 export const fetch_all_players_from_team = async (team_id: string, year: string): Promise<playerSchema[]> => {
-	const response = await customFetch(`${API_BASE}/players/team/${team_id}/${year}`);
+	const response = await customFetch(`/players/team/${team_id}/${year}`);
 
 	return response.json();
 };
 
 export const fetch_player_history = async (player_id: string): Promise<playerTeamHistory[]> => {
-	const response = await customFetch(`${API_BASE}/player_teams/team_history/${player_id}`);
+	const response = await customFetch(`/player_teams/team_history/${player_id}`);
 
 	return response.json();
 };
