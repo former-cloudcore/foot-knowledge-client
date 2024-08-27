f779dcb5a3994621f8281da85a6e671fe81a9aa2
Eyal Hermoni <323012633@idf.il>
2024-08-27 16:05:26 +0300

    a

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,8 +1,8 @@
 import data from './defaultPresets.json';
 import { filterSchema } from './api.interfaces';
 
-export const API_BASE = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : '';
-export const AUTH_API_BASE = import.meta.env.MODE === 'development' ? 'http://localhost:4000/api' : '';
+export const API_BASE = import.meta.env.MODE === 'development' ? ':3000' : '';
+export const AUTH_API_BASE = import.meta.env.MODE === 'development' ? ':4000/api' : '';
 
 export const top_left_image = 'https://www.the-sun.com/wp-content/uploads/sites/6/2022/01/NINTCHDBPICT000283898937.jpg';
 
