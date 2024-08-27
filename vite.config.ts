d3305ef25bb40c6dfa6bc54791e5c39621140d85
Eyal Hermoni <323012633@idf.il>
2024-08-27 15:58:23 +0300

    ports

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    host: '0.0.0.0',
-  },
+	plugins: [react()],
+	server: {
+		open: true,
+		host: '0.0.0.0',
+		port: 80,
+	},
 });
